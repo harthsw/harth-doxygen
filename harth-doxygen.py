b2acbb1a44997fc041a19ba2279cb7e56ad544c0
Harvey Thompson <harv.thompson@icloud.com>
2016-03-07 12:31:19 -0800

    More detail on reading index and namespaces. HARTH-1604

--- harth-doxygen.py
+++ harth-doxygen.py
@@ -14,10 +14,11 @@ sys.path.append("%%LIBDIR%%")
 # Class names and strcture almost match those in Doxygen's XSD:
 # See: ~/Projects/harth-application/dep/harth-kernel/build/xml/index.xsd
 
-class XMLDoxygenIndex:
-    def __init__(self, tree):
-        self.tree = tree
-        self.root = tree.getroot()
+class DoxygenIndex:
+    def __init__(self, path):
+        self.path = path
+        self.tree = ET.parse(path)
+        self.root = self.tree.getroot()
 
     @property
     def version(self):
@@ -27,7 +28,7 @@ class XMLDoxygenIndex:
     def compounds(self):
         return self.root.iter("compound")
 
-class XMLCompound:
+class Compound:
     def __init__(self, elem):
         self.elem = elem
 
@@ -43,7 +44,7 @@ class XMLCompound:
     def names(self):
         return self.elem.findall("name")
 
-class XMLName:
+class Name:
     def __init__(self, elem):
         self.elem = elem
 
@@ -51,28 +52,29 @@ class XMLName:
     def text(self):
         return self.elem.text
 
-class XMLDoxygen:
-    def __init__(self, tree):
-        self.tree = tree
-        self.root = tree.getroot()
+class DoxygenDefIndex:
+    def __init__(self, path):
+        self.path = path
+        self.tree = ET.parse(path)
+        self.root = self.tree.getroot()
 
     @property
     def defs(self):
         return self.root.iter("compounddef")
 
-class XMLCompoundDef:
+class CompoundDef:
     def __init__(self, elem):
         self.elem = elem
 
     @property
     def name(self):
-        return XMLName(self.elem.find("compoundname"))
+        return Name(self.elem.find("compoundname"))
 
     @property
     def location(self):
-        return XMLLocation(self.elem.find("location"))
+        return Location(self.elem.find("location"))
 
-class XMLLocation:
+class Location:
     def __init__(self, elem):
         self.elem = elem
     
@@ -87,43 +89,45 @@ class XMLLocation:
     @property
     def column(self):
         return self.elem.attrib["column"]
+
+class NamespaceDef(CompoundDef):
+    def __init__(self, elem):
+        self.elem = elem
+
+    @property
+    def child_namespaces(self):
+        return self.elem.findall("innernamespace")
     
 # --------------------------------------------------------------------------------
 
-xml = "../harth-application/dep/harth-kernel/build/xml/index.xml"
-tree = ET.parse(xml)
 
-def_xml = "../harth-application/dep/harth-kernel/build/xml/struct_harth_1_1_kernel_1_1_meta_1_1_all_helper.xml"
-def_tree = ET.parse(def_xml)
-
-index = XMLDoxygenIndex(tree)
-def_index = XMLDoxygen(def_tree)
+index = DoxygenIndex("../harth-application/dep/harth-kernel/build/xml/index.xml")
 
 print "Index"
-print "Version:", index.version
-print "Compounds:"
+print "  Version:", index.version
+print "  Compounds:"
 
 for c in index.compounds:
-    compound = XMLCompound(c)
+    compound = Compound(c)
 
-    print "  refid=", compound.refid
-    print "  kind=", compound.kind
-    for child in compound.elem._children:
-        print "   child.tag=", child.tag
-        print "   child.text=", child.text
-    print "  names=[" 
-    for n in compound.names:
-        print "    ", n.text
-    print "  ]"
+    if compound.kind == "namespace":
+        print "  refid=", compound.refid
+        print "  kind=", compound.kind
+        
+        # for child in compound.elem._children:
+        #     print "   child.tag=", child.tag
+        #     print "   child.text=", child.text
+        print "  names=[" 
+        for n in compound.names:
+            print "    ", n.text
+        print "  ]"
 
-    for d in def_index.defs:
-        compoundDef = XMLCompoundDef(d)
-        name = compoundDef.name
-        loc = compoundDef.location        
-        print "Def Name=", name.text
-        print "At ", loc.file, loc.line, loc.column
-    sys.exit(0)
+def_index = DoxygenDefIndex("../harth-application/dep/harth-kernel/build/xml/namespace_harth.xml")
+for ns in def_index.defs:
+    def_ns = NamespaceDef(ns)
 
-    
-
-
+    print "ns=", def_ns.name.text
+    print "loc=", def_ns.location.file, def_ns.location.line
+    print "children=", def_ns.child_namespaces
+            
+sys.exit(0)
